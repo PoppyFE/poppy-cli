79c19584bd90d32d9960ac10cd9810c49c6cf6f6
alex zhang <gzane24@gmail.com>
2019-02-28 18:10:51 +0800

    add  files

--- src/commands/sync.js
+++ src/commands/sync.js
@@ -1,33 +1,27 @@
 const program = require('commander');
-const gitDownload = require('git-download');
+const gitPullOrClone = require('ensure-git-repo');
+const fs = require('fs');
 const pkg = require('../../package.json');
+const path = require('path');
+const os = require('os');
+const copyfiles = require('copyfiles2');
 
 // command args '<>' is must
 // command args '[]' is options
 program
-  .command('sync <project_name> <dir>')
+  .command('sync <project_name> <dest_dir>')
   .description('sync the project from poppy repo')
   .option('-g, --ignore', 'ignore the .poppyrc file')
-  .action(function(project_name, dir) {
+  .option('-v, --verbose', 'verbose the progress')
+  .action(async function(projectName, destDir, opts) {
     // console.log('sdfsdfsadadasdasd');
     // console.log('exec "%s" using %s mode', value, options.exec_mode);
-    console.log(project_name, dir)
+    // console.log(project_name, dir);
+    //
+    const poppyDir = path.join(os.homedir(), '.poppy');
+    await syncRepo(poppyDir, projectName);
+    await copyFiles(poppyDir, projectName, destDir, opts.ignore);
 
-    gitDownload({
-      source: 'ssh://git@github.com:ValueFE/poppy-cli.git',
-      branch: 'master:' + project_name,
-      savetar: false,
-      dest: '/Users/alex/Projects/test/aaa'
-    }, function(err, tarfile) {
-      if (err) {
-        console.error('Error occurred downloading '+err);
-        return;
-      }
-      console.log('Successfully downloaded ');
-      if (tarfile) {
-        console.log('Output: '+tarfile);
-      }
-    });
   })
   .on('--help', function() {
     console.log('  Example:');
@@ -35,3 +29,68 @@ program
     console.log('');
     console.log('');
   });
+
+
+async function syncRepo(poppyDir, projectName) {
+  await new Promise((resolve, reject)=>{
+    gitPullOrClone('git@e.coding.net:value/ifpay-poppy.git',
+      poppyDir, err => {
+        if (err) {
+          console.error(err.stack);
+          reject(reject);
+          return;
+        }
+
+        if (!fs.existsSync(path.join(poppyDir, projectName))) {
+          reject(new Error(`Error projectName (${projectName}) Error Not Exists`));
+          return;
+        }
+
+        resolve();
+      });
+  });
+}
+
+async function copyFiles(poppyDir, projectName, destDir, ignore) {
+  // 读取 ignore file
+  if (!destDir) {
+    destDir = '.';
+  }
+
+  if (!destDir.startsWith('/') &&
+    !destDir.startsWith('~')) {
+    destDir = path.join(process.cwd(), destDir);
+  }
+
+  const projectDir = path.join(poppyDir, projectName);
+
+  console.info(`[poppy-cli] sync project ${projectName} to ${destDir}`);
+
+  const opts = {
+    soft: false,
+    // verbose: true,
+    all: true,
+    up: function(fromPath) {
+      return fromPath.substring(projectDir.length + 1);
+    },
+  };
+
+  const ignoreFilePath = path.join(destDir, '.poppyignore');
+  if (!ignore && fs.existsSync(ignoreFilePath)) {
+    const contents = fs.readFileSync(ignoreFilePath, 'utf8');
+    opts.exclude = contents;
+  }
+
+  await new Promise((resolve, reject)=>{
+    copyfiles([
+      projectDir + '/**',
+      destDir
+    ], opts, (err)=> {
+      if (err) {
+        reject();
+        return;
+      }
+      resolve();
+    });
+  });
+}
